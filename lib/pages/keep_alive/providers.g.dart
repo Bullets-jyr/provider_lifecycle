592c36cf10f79776a06e7b9a9694c87ba52e8dc8
Bullets-jyr <jyr@bullets.co.kr>
2024-01-29 13:39:48 +0900

    61. Lifecycle of keepAlive provider

---
+++ lib/pages/keep_alive/providers.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$keepAliveCounterHash() => r'0f8ae044cf50c69824f004a1681f43df754e7977';
+
+/// See also [KeepAliveCounter].
+@ProviderFor(KeepAliveCounter)
+final keepAliveCounterProvider =
+    NotifierProvider<KeepAliveCounter, int>.internal(
+  KeepAliveCounter.new,
+  name: r'keepAliveCounterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$keepAliveCounterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$KeepAliveCounter = Notifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
